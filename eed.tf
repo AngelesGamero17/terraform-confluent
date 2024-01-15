93c8aada1f63253374c8b58b6124205d20877990
AngelesGamero17 <angelesgamero17@outlook.com>
2024-01-15 17:58:15 -0500

    segundo commit del archivo eed

---
+++ eed.tf
@@ -0,0 +1,17 @@
+
+resource "confluent_kafka_topic" "topic_eed_0" {
+  kafka_cluster {
+    id = "lkc-w2oxdg"
+  }
+  topic_name         = "topic_eed_0"
+  rest_endpoint      = "https://pkc-4r087.us-west2.gcp.confluent.cloud:443"
+  partitions_count = 2
+  credentials {
+    key    = "UGERBGP2N54H4V74"
+    secret = "08U7i4hfI2er+S0/CKrvblJ6s+Lavc1hodY2NVH1IFrQyzakvjoB1tXzkycRe+IH"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
