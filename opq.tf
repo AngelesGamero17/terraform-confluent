0eed7f1a7eb10e2c92bc5b4da8490110193ed908
AngelesGamero17 <angelesgamero17@outlook.com>
2024-01-15 16:50:24 -0500

    archivo opq

--- opq.tf
+++ opq.tf
@@ -1,9 +1,9 @@
 
-resource "confluent_kafka_topic" "topic_xyz_0" {
+resource "confluent_kafka_topic" "topic_opq_0" {
   kafka_cluster {
     id = "lkc-w2oxdg"
   }
-  topic_name         = "topic_xyz_0"
+  topic_name         = "topic_opq_0"
   rest_endpoint      = "https://pkc-4r087.us-west2.gcp.confluent.cloud:443"
   partitions_count = 1
   credentials {
