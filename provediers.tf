1c701315f2d834436b1d4cda6c88cadcb32ee211
AngelesGamero17 <angelesgamero17@outlook.com>
2024-01-15 14:43:35 -0500

    primer commit donde ya hay un topico

---
+++ provediers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    confluent = {
+      source = "confluentinc/confluent"
+
+    }
+  }
+}
+
+provider "confluent" {
+  cloud_api_key    = "UGERBGP2N54H4V74"
+  cloud_api_secret = "08U7i4hfI2er+S0/CKrvblJ6s+Lavc1hodY2NVH1IFrQyzakvjoB1tXzkycRe+IH"
+}
